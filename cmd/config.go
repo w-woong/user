6c9786575d219200c42944792c0df7e1787c9ac9
siabba15 <siabba15@daiso.co.kr>
2022-10-17 16:09:27 +0900

    refactor: main

---
+++ cmd/config.go
@@ -0,0 +1,59 @@
+package main
+
+import "encoding/json"
+
+type Config struct {
+	Server ConfigServer `mapstructure:"server"`
+	Logger ConfigLogger `mapstructure:"logger"`
+}
+
+func (c *Config) String() string {
+	b, _ := json.Marshal(c)
+	return string(b)
+}
+
+type ConfigServer struct {
+	Http ConfigHttp `mapstructure:"http"`
+	Repo ConfigRepo `mapstructure:"repo"`
+}
+
+type ConfigHttp struct {
+	Timeout        int       `mapstructure:"timeout"`
+	HmacHeader     string    `mapstructure:"hmac_header"`
+	HmacSecret     string    `mapstructure:"hmac_secret"`
+	BearerToken    string    `mapstructure:"bearer_token"`
+	Jwt            ConfigJwt `mapstructure:"jwt"`
+	AllowedOrigins string    `mapstructure:"allowed_origins"`
+	AllowedHeaders string    `mapstructure:"allowed_headers"`
+	AllowedMethods string    `mapstructure:"allowed_methods"`
+}
+
+type ConfigJwt struct {
+	Secret          string `mapstructure:"secret"`
+	AccessTokenExp  int    `mapstructure:"access_token_exp"`
+	RefreshToken    bool   `mapstructure:"refresh_token"`
+	RefreshTokenExp int    `mapstructure:"refresh_token_exp"`
+}
+
+type ConfigRepo struct {
+	Driver                 string `mapstructure:"driver"`
+	ConnStr                string `mapstructure:"conn_str"`
+	MaxIdleConns           int    `mapstructure:"max_idle_conns"`
+	MaxOpenConns           int    `mapstructure:"max_open_conns"`
+	ConnMaxLifetimeMinutes int    `mapstructure:"conn_max_lifetime_in_min"`
+}
+
+type ConfigLogger struct {
+	Json   bool       `mapstructure:"json"`
+	Stdout bool       `mapstructure:"stdout"`
+	File   ConfigFile `mapstructure:"file"`
+	Level  string     `mapstructure:"level"`
+}
+
+type ConfigFile struct {
+	Name       string `mapstructure:"name"`
+	MaxSize    int    `mapstructure:"max_size"`
+	MaxBackup  int    `mapstructure:"max_backup"`
+	MaxAge     int    `mapstructure:"max_age"`
+	Compressed bool   `mapstructure:"compressed"`
+}
--- cmd/main.go
+++ cmd/main.go
@@ -1,96 +1,154 @@
 package main
 
 import (
-	"context"
-	"database/sql"
+	"crypto/tls"
 	"flag"
-	"log"
-	"net/http"
+	"fmt"
 	"os"
-	"os/signal"
+	"runtime"
+	"strings"
 	"syscall"
 	"time"
 
+	"github.com/go-wonk/si"
+	"github.com/go-wonk/si/sigorm"
+	"github.com/go-wonk/si/sihttp"
 	"github.com/gorilla/mux"
+	"github.com/w-woong/common"
+	"github.com/w-woong/common/configs"
+	"github.com/w-woong/common/logger"
 	"github.com/w-woong/user/adapter"
 	"github.com/w-woong/user/delivery"
 	"github.com/w-woong/user/entity"
+	"github.com/w-woong/user/port"
 	"github.com/w-woong/user/usecase"
-	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 var (
-	addr string
+	Version = "undefined"
+
+	printVersion     bool
+	tickIntervalSec  int = 30
+	addr             string
+	certPem, certKey string
+	readTimeout      int
+	writeTimeout     int
+	configName       string
+	maxProc          int
+
+	usePprof  = false
+	pprofAddr = ":56060"
 )
 
 func init() {
 	flag.StringVar(&addr, "addr", ":8080", "listen address")
+	flag.BoolVar(&printVersion, "version", false, "print version")
+	flag.IntVar(&tickIntervalSec, "tick", 30, "tick interval in second")
+	flag.StringVar(&certKey, "key", "", "server key")
+	flag.StringVar(&certPem, "pem", "", "server pem")
+	flag.IntVar(&readTimeout, "readTimeout", 30, "read timeout")
+	flag.IntVar(&writeTimeout, "writeTimeout", 30, "write timeout")
+	flag.StringVar(&configName, "config", "./configs/server.yml", "config file name")
+	flag.IntVar(&maxProc, "mp", runtime.NumCPU(), "GOMAXPROCS")
+
+	flag.BoolVar(&usePprof, "pprof", false, "use pprof")
+	flag.StringVar(&pprofAddr, "pprof_addr", ":56060", "pprof listen address")
+
 	flag.Parse()
 }
 
 func main() {
 	defaultTimeout := 6 * time.Second
-	dsn := "host=testpghost user=test password=test123 dbname=woong_user port=5432 sslmode=disable TimeZone=Asia/Seoul"
-	var sqlDB *sql.DB
+
 	var err error
-	if sqlDB, err = sql.Open("pgx", dsn); err != nil {
-		log.Println(err)
+
+	if printVersion {
+		fmt.Printf("version \"%v\"\n", Version)
+		return
+	}
+	runtime.GOMAXPROCS(maxProc)
+
+	// config
+	conf := Config{}
+	if err := configs.ReadConfigInto(configName, &conf); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	// logger
+	logger.Open(conf.Logger.Level, conf.Logger.Stdout, conf.Logger.File.Name, conf.Logger.File.MaxSize, conf.Logger.File.MaxBackup, conf.Logger.File.MaxAge, conf.Logger.File.Compressed)
+	defer logger.Close()
+
+	// db
+	sqlDB, err := si.OpenSqlDB(conf.Server.Repo.Driver, conf.Server.Repo.ConnStr,
+		conf.Server.Repo.MaxIdleConns, conf.Server.Repo.MaxOpenConns, time.Duration(conf.Server.Repo.ConnMaxLifetimeMinutes)*time.Minute)
+	if err != nil {
+		fmt.Println(err)
 		os.Exit(1)
 	}
 	defer sqlDB.Close()
-	sqlDB.SetMaxIdleConns(10)
-	sqlDB.SetMaxOpenConns(100)
-	sqlDB.SetConnMaxLifetime(15 * time.Minute)
 
-	db, err := gorm.Open(postgres.New(postgres.Config{
-		Conn: sqlDB,
-	}), &gorm.Config{})
+	// gorm
+	var gormDB *gorm.DB
+	switch conf.Server.Repo.Driver {
+	case "pgx":
+		gormDB, err = sigorm.OpenPostgres(sqlDB)
+	default:
+		logger.Error(conf.Server.Repo.Driver + " is not allowed")
+		os.Exit(1)
+	}
 	if err != nil {
-		log.Println(err)
+		logger.Error(err.Error())
 		os.Exit(1)
 	}
 
-	db.AutoMigrate(&entity.User{}, &entity.UserEmail{})
+	gormDB.AutoMigrate(&entity.User{}, &entity.UserEmail{})
 
-	// userRepo := adapter.NewNopUser()
-	txBeginner := adapter.NewGormTxBeginner(db)
-	userRepo := adapter.NewPgUser(db)
-	userUsc := usecase.NewUser(txBeginner, userRepo, defaultTimeout)
-	userDelivery := delivery.NewUserHttpHandler(userUsc)
-
-	router := mux.NewRouter()
-	router.HandleFunc("/v1/user/register", userDelivery.HandleRegisterUser).Methods("POST")
-	router.HandleFunc("/v1/user/{id}", userDelivery.HandleFindByID).Methods("GET")
-	router.HandleFunc("/v1/user/{id}", userDelivery.HandleChangeUser).Methods("PUT")
-	router.HandleFunc("/v1/user/{id}", userDelivery.HandleRemoveUser).Methods("DELETE")
-
-	server := http.Server{
-		Addr:    addr,
-		Handler: router,
-	}
-
-	go startSyscallChecker(&server)
-
-	if err := server.ListenAndServe(); err != nil {
-		log.Println(err)
-	}
-
-}
-
-func startSyscallChecker(hs *http.Server) {
-	sigs := make(chan os.Signal, 1)
-	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
-
-	sig := <-sigs
-	switch sig {
-	case syscall.SIGINT:
-		log.Println("syscall.SIGINT")
-	case syscall.SIGTERM:
-		log.Println("syscall.SIGTERM")
+	var userUsc port.UserUsc
+	switch conf.Server.Repo.Driver {
+	case "pgx":
+		txBeginner := adapter.NewGormTxBeginner(gormDB)
+		userRepo := adapter.NewPgUser(gormDB)
+		userUsc = usecase.NewUser(txBeginner, userRepo, defaultTimeout)
 	default:
-		log.Printf("signal %v", sig)
+		logger.Error(conf.Server.Repo.Driver + " is not allowed")
+		os.Exit(1)
 	}
-	hs.Shutdown(context.Background())
 
+	// http handler
+	userHandler = delivery.NewUserHttpHandler(userUsc)
+	router := mux.NewRouter()
+	SetRoute(router, conf.Server.Http)
+
+	// http server
+	tlsConfig := sihttp.CreateTLSConfigMinTls(tls.VersionTLS12)
+	httpServer := sihttp.NewServerCors(router, tlsConfig, addr,
+		time.Duration(writeTimeout)*time.Second, time.Duration(readTimeout)*time.Second,
+		certPem, certKey,
+		strings.Split(conf.Server.Http.AllowedOrigins, ","),
+		strings.Split(conf.Server.Http.AllowedHeaders, ","),
+		strings.Split(conf.Server.Http.AllowedMethods, ","),
+	)
+
+	// ticker
+	ticker := time.NewTicker(time.Duration(tickIntervalSec) * time.Second)
+	tickerDone := make(chan bool)
+	common.StartTicker(tickerDone, ticker, func(t time.Time) {
+		logger.Info(fmt.Sprintf("NoOfGR:%v, %v", runtime.NumGoroutine(), t))
+	})
+
+	// signal, wait for it to shutdown http server.
+	common.StartSignalStopper(httpServer, syscall.SIGINT, syscall.SIGTERM)
+
+	// start
+	logger.Info("start listening on " + addr)
+	if err = httpServer.Start(); err != nil {
+		logger.Error(err.Error())
+	}
+
+	// finish
+	ticker.Stop()
+	tickerDone <- true
+	logger.Info("finished")
 }
